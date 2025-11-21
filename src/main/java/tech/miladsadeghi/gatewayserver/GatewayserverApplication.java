22b20488539a0f3ae8a1c5f602dec3d800352709
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-21 18:33:13 +0330

    Add custom route locator for microservices in gateway application

--- src/main/java/tech/miladsadeghi/gatewayserver/GatewayserverApplication.java
+++ src/main/java/tech/miladsadeghi/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,36 @@ package tech.miladsadeghi.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayserverApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayserverApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/microservices/accounts/**")
+                        .filters(f -> f.rewritePath("/microservices/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS")
+                )
+                .route(p -> p
+                        .path("/microservices/cards/**")
+                        .filters(f -> f.rewritePath("/microservices/cards/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://CARDS")
+                )
+                .route(p -> p
+                        .path("/microservices/loans/**")
+                        .filters(f -> f.rewritePath("/microservices/loans/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://LOANS")
+                )
+                .build();
     }
 
 }
