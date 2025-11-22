2efa21f5f876163cce65ee4bdbac9ea15f1ac943
Milad Sadeghi <MiladSadeghiDM@Gmail.com>
2025-11-22 17:31:46 +0330

    Add response time header to microservices routes in gateway

--- src/main/java/tech/miladsadeghi/gatewayserver/GatewayserverApplication.java
+++ src/main/java/tech/miladsadeghi/gatewayserver/GatewayserverApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -18,17 +21,41 @@ public class GatewayserverApplication {
         return builder.routes()
                 .route(p -> p
                         .path("/microservices/accounts/**")
-                        .filters(f -> f.rewritePath("/microservices/accounts/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/microservices/accounts/(?<segment>.*)", "/${segment}")
+                                .modifyResponseBody(String.class, String.class,
+                                        (exchange, body) -> {
+                                            exchange.getResponse().getHeaders()
+                                                    .add("X-Response-Time", LocalDateTime.now().toString());
+                                            return Mono.just(body);
+                                        })
+                        )
                         .uri("lb://ACCOUNTS")
                 )
                 .route(p -> p
                         .path("/microservices/cards/**")
-                        .filters(f -> f.rewritePath("/microservices/cards/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/microservices/cards/(?<segment>.*)", "/${segment}")
+                                .modifyResponseBody(String.class, String.class,
+                                        (exchange, body) -> {
+                                            exchange.getResponse().getHeaders()
+                                                    .add("X-Response-Time", LocalDateTime.now().toString());
+                                            return Mono.just(body);
+                                        })
+                        )
                         .uri("lb://CARDS")
                 )
                 .route(p -> p
                         .path("/microservices/loans/**")
-                        .filters(f -> f.rewritePath("/microservices/loans/(?<segment>.*)", "/${segment}"))
+                        .filters(f -> f
+                                .rewritePath("/microservices/loans/(?<segment>.*)", "/${segment}")
+                                .modifyResponseBody(String.class, String.class,
+                                        (exchange, body) -> {
+                                            exchange.getResponse().getHeaders()
+                                                    .add("X-Response-Time", LocalDateTime.now().toString());
+                                            return Mono.just(body);
+                                        })
+                        )
                         .uri("lb://LOANS")
                 )
                 .build();
